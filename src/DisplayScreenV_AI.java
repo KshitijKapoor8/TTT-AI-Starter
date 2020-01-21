f031cd3d6bcd373d540eb7d21fd6e7933c073017
OGBeturtle <59973616+OGBeturtle@users.noreply.github.com>
2020-01-21 10:21:47 -0600

    Update DisplayScreenV_AI.java

--- src/DisplayScreenV_AI.java
+++ src/DisplayScreenV_AI.java
@@ -24,6 +24,7 @@ public class DisplayScreenV_AI extends JFrame implements Runnable
 	public void paint(Graphics g)
 	{
 		board.draw(img.getGraphics());
+        //yeet-----------------------------------------------------------------------------
 
 		g.drawImage(img,0,0,null);
 	}
