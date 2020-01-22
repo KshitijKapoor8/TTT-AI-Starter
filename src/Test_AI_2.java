ccbf4adb16b048b68a30b88cc5bf7a7a0a157cf2
OGBeturtle <59973616+OGBeturtle@users.noreply.github.com>
2020-01-22 10:37:35 -0600

    y

--- src/Test_AI_2.java
+++ src/Test_AI_2.java
@@ -22,18 +22,29 @@ public class Test_AI_2 implements PlayerInt
 	// Post: 	returns the Location where the player wants to move
 	public Location getMove(Board board)
 	{
-		Location l;
+		Location l = null;
 		Random rand = new Random();
 		do
 		{
 			for(int r = 0 ; r < board.numRows() ; r++) {
 				for(int c = 0 ; c < board.numCols() ; c++) {
 					for(int d = 0 ; d < board.numSheets() ; d++) {
-						if(board.getLocation(new Location(r, c, d)) == )
+						if(board.getLocation(new Location(r, c, d)) != getLetter()) {
+							if(r == 3 || c == 3 || d == 3) {
+								l = new Location(r-1, c-1, d-1);
+							}
+							else {
+								l = new Location(r+1, c, d+1);
+							}
+							break;
+						}
+						else {
+							l = new Location(rand.nextInt(4),rand.nextInt(4),rand.nextInt(4));
+							break;
+						}
 					}
 				}
 			}
-			l = new Location(rand.nextInt(4),rand.nextInt(4),rand.nextInt(4));
 		}while(!board.isEmpty(l));
 		return l;
 	}
