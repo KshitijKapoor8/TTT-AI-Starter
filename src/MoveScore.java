54d31f7cbe1660c777c195a3a1db8c8439ddbbac
Kshitij Kapoor <35787705+KshitijKapoor8@users.noreply.github.com>
2020-01-22 10:38:52 -0600

    y

--- src/MoveScore.java
+++ src/MoveScore.java
@@ -1,8 +1,24 @@
+
 public class MoveScore {
-    // Stuff
-    // Ian is yeetus deletus
-    // hi
-    // test
-    //Ian is yeetus deletus
-    //yeetur
+    private int x;
+    private int y;
+    private int z;
+    private int board;
+
+    public MoveScore(int x, int y, int z, int board) {
+
+    }
+
+    public void gradeMove(int x, int y, int z, Board board, char player)
+    {
+        int count = 1;
+
+        for (int i = 0; i < board.numCols(); i++)
+        {
+            if (board.getData()[new Location(x, y, z).getSheet()][new Location(x, y, z).getRow()][i] == player)
+            {
+                count++;
+            }
+        }
+    }
 }
