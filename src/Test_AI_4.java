95b71c67c993a769f02853af0db37dfbf7025527
Kshitij Kapoor <35787705+KshitijKapoor8@users.noreply.github.com>
2020-01-24 11:38:33 -0600

    y

--- src/Test_AI_4.java
+++ src/Test_AI_4.java
@@ -3,14 +3,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
-public class Test_AI_3 implements PlayerInt
+public class Test_AI_4 implements PlayerInt
 {
     private char letter;
     private String name;
 
     // Pre:		receives a char letter
     // Post: 	sets the name to "Random AI" and the letter to the letter received
-    Test_AI_3(char letter)
+    Test_AI_4(char letter)
     {
         name 		= "ScoreTest1";
         this.letter	= letter;
@@ -27,9 +27,32 @@ public class Test_AI_3 implements PlayerInt
     {
         Location l;
         Location greatest = new Location(0, 0, 0);
-        Random rand = new Random();
-        ArrayList<Integer> test = new ArrayList<>();
+//        Random rand = new Random();
+//        ArrayList<Integer> test = new ArrayList<>();
+//
+//        System.out.println(greatest);
+//        System.out.println(compare[greatest.getSheet()][greatest.getRow()][greatest.getCol()]);
+        l = greatest;
+        return l;
+    }
+
+    // Pre:		method is called
+    // Post: 	returns the name of the player
+    public String getName()
+    {
+        return name;
+    }
+
+    public PlayerInt freshCopy()
+    {
+        return new Test_AI_1(letter);
+    }
+
+    public Board gradeBoard(Board board)
+    {
+
         int[][][] compare = new int[4][4][4];
+        Location greatest = new Location(0, 0, 0);
         for (int x = 0; x < board.getData().length; x++)
         {
             for (int y = 0; y < board.getData()[0].length; y++)
@@ -40,7 +63,6 @@ public class Test_AI_3 implements PlayerInt
                         continue;
                     }
                     compare[x][y][z] = gradeMove(new Location(x, y, z), board, 'x', 'o');
-                    test.add(compare[x][y][z]);
                 }
             }
         }
@@ -60,24 +82,8 @@ public class Test_AI_3 implements PlayerInt
             }
         }
 
-//        System.out.println(greatest);
-//        System.out.println(compare[greatest.getSheet()][greatest.getRow()][greatest.getCol()]);
-        l = greatest;
-        return l;
+        return board;
     }
-
-    // Pre:		method is called
-    // Post: 	returns the name of the player
-    public String getName()
-    {
-        return name;
-    }
-
-    public PlayerInt freshCopy()
-    {
-        return new Test_AI_1(letter);
-    }
-
     public int gradeMove(Location l, Board board, char player, char opponent)
     {
         if (board.getData()[l.getSheet()][l.getRow()][l.getCol()] == '-')
@@ -92,55 +98,41 @@ public class Test_AI_3 implements PlayerInt
                 if (board.getData()[l.getSheet()][l.getRow()][i] == player) {
                     System.out.println("here");
                     count++;
-                    if (count == 4) {
-                        my4s++;
-                    }
                 } else if (board.getData()[l.getSheet()][l.getRow()][i] == opponent) {
-
+                    System.out.println("bad");
                     count = 0;
                     break;
                 }
-
+                if (count == 3) {
+                    my4s++;
+                } else if (count == 2) {
+                    my3s++;
+                } else if (count == 1) {
+                    my2s++;
+                }
             }
 
             for (int i = 0; i < board.numRows(); i++) {
                 if (board.getData()[l.getSheet()][i][l.getCol()] == player) {
                     System.out.println("here");
                     count++;
-                    if (count == 4) {
-                        my4s++;
-                    }
                 } else if (board.getData()[l.getSheet()][i][l.getCol()] == opponent) {
                     System.out.println("bad");
                     count = 0;
                     break;
                 }
-
-            }
-
-            for (int i = 0; i < board.numSheets(); i++) {
-                if (board.getData()[i][l.getRow()][l.getCol()] == player) {
-                    System.out.println("here");
-                    count++;
-                    if (count == 4) {
-                        my4s++;
-                    }
-                } else if (board.getData()[i][l.getRow()][l.getCol()] == opponent) {
-                    System.out.println("bad");
-                    count = 0;
-                    break;
+                if (count == 3) {
+                    my4s++;
+                } else if (count == 2) {
+                    my3s++;
+                } else if (count == 1) {
+                    my2s++;
                 }
             }
-//            if (count == 4) {
-//                my4s++;
-//            } else if (count == 3) {
-//                my3s++;
-//            } else if (count == 2) {
-//                my2s++;
-//            }
-            System.out.println(my4s + " " + my3s + " " + my2s);
 
-            return (my4s * 100) + (my3s * 50) + (my2s * 12);
+
+
+            return (my4s * 100) + (my3s * 10) + (my2s * 3);
         }
         else
         {
