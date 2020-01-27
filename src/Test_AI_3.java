e0a2c234c904d69d367c7dcb408adee8f16820c9
Kshitij Kapoor <35787705+KshitijKapoor8@users.noreply.github.com>
2020-01-27 11:37:55 -0600

    y

--- src/Test_AI_3.java
+++ src/Test_AI_3.java
@@ -30,6 +30,25 @@ public class Test_AI_3 implements PlayerInt
         Random rand = new Random();
         ArrayList<Integer> test = new ArrayList<>();
         int[][][] compare = new int[4][4][4];
+
+        int count = 0;
+        for (int x = 0; x < board.getData().length; x++)
+        {
+            for (int y = 0; y < board.getData()[0].length; y++)
+            {
+                for (int z = 0 ; z < board.getData()[0][0].length; z++) {
+                    if (board.getData()[x][y][z] != '-')
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        if (count == 0)
+        {
+            return new Location(1,2,3);
+        }
+
         for (int x = 0; x < board.getData().length; x++)
         {
             for (int y = 0; y < board.getData()[0].length; y++)
@@ -53,15 +72,14 @@ public class Test_AI_3 implements PlayerInt
                 {
                     if (compare[x][y][z] >= compare[greatest.getCol()][greatest.getRow()][greatest.getSheet()] && (board.getData()[x][y][z] != 'x' || board.getData()[x][y][z] != 'o'))
                     {
-                        System.out.println(board.getData()[x][y][z]);
                         greatest = new Location(x, y, z);
                     }
                 }
             }
         }
 
-//        System.out.println(greatest);
-//        System.out.println(compare[greatest.getSheet()][greatest.getRow()][greatest.getCol()]);
+        System.out.println(greatest);
+        System.out.println(compare[greatest.getSheet()][greatest.getRow()][greatest.getCol()]);
         l = greatest;
         return l;
     }
@@ -85,19 +103,18 @@ public class Test_AI_3 implements PlayerInt
             int my4s = 0;
             int my3s = 0;
             int my2s = 0;
-            int count = 1;
+            int count1 = 1;
+            int count2 = 1;
+            int count3 = 1;
             int totalScore = 0;
 
             for (int i = 0; i < board.numCols(); i++) {
                 if (board.getData()[l.getSheet()][l.getRow()][i] == player) {
                     System.out.println("here");
-                    count++;
-                    if (count == 4) {
-                        my4s++;
-                    }
-                } else if (board.getData()[l.getSheet()][l.getRow()][i] == opponent) {
+                    count1++;
 
-                    count = 0;
+                } else if (board.getData()[l.getSheet()][l.getRow()][i] == opponent) {
+                    count1 = 0;
                     break;
                 }
 
@@ -106,13 +123,11 @@ public class Test_AI_3 implements PlayerInt
             for (int i = 0; i < board.numRows(); i++) {
                 if (board.getData()[l.getSheet()][i][l.getCol()] == player) {
                     System.out.println("here");
-                    count++;
-                    if (count == 4) {
-                        my4s++;
-                    }
+                    count2++;
+
                 } else if (board.getData()[l.getSheet()][i][l.getCol()] == opponent) {
                     System.out.println("bad");
-                    count = 0;
+                    count2 = 0;
                     break;
                 }
 
@@ -121,30 +136,40 @@ public class Test_AI_3 implements PlayerInt
             for (int i = 0; i < board.numSheets(); i++) {
                 if (board.getData()[i][l.getRow()][l.getCol()] == player) {
                     System.out.println("here");
-                    count++;
-                    if (count == 4) {
-                        my4s++;
-                    }
+                    count3++;
+
                 } else if (board.getData()[i][l.getRow()][l.getCol()] == opponent) {
                     System.out.println("bad");
-                    count = 0;
+                    count3 = 0;
                     break;
                 }
             }
-//            if (count == 4) {
-//                my4s++;
-//            } else if (count == 3) {
-//                my3s++;
-//            } else if (count == 2) {
-//                my2s++;
-//            }
-            System.out.println(my4s + " " + my3s + " " + my2s);
-
-            return (my4s * 100) + (my3s * 50) + (my2s * 12);
+            if (count1 == 4) {
+                my4s++;
+            } else if (count1 == 3) {
+                my3s++;
+            } else if (count1 == 2) {
+                my2s++;
+            }
+            if (count2 == 4) {
+                my4s++;
+            } else if (count2 == 3) {
+                my3s++;
+            } else if (count2 == 2) {
+                my2s++;
+            }
+            if (count3 == 4) {
+                my4s++;
+            } else if (count3 == 3) {
+                my3s++;
+            } else if (count3 == 2) {
+                my2s++;
+            }
+            return (my4s * 100) + (my3s * 20) + (my2s * 7);
         }
         else
         {
-            return 0;
+            return -1;
         }
     }
 }
